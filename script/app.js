9b84d78b6ea929a3a221b7eeb9a28e99fe840dfb
Marian Horoiu <horoiu@yahoo.com>
2018-04-05 23:52:29 +0300

    Create basic HTML layout. Work on 'Header': add buttons and implement JavaScript for functionalities.

--- script/app.js
+++ script/app.js
@@ -1,1 +1,88 @@
-console.log("Application started!");
+console.log("JS LOADED");
+
+
+
+
+
+
+
+let UIController  = (function() {
+
+    let DOMStrings = {
+        loginBtn: document.querySelector('.header__btn-box--login'),
+        logoutBtn: document.querySelector('.header__btn-box--logout'),
+        registerBtn: document.querySelector('.header__btn-box--register'),
+
+    };
+
+    return {
+        getDOMStrings: function() {
+            return DOMStrings;
+        },
+    }
+
+})();
+
+
+
+
+
+let userController  = (function() {
+
+    let loginRequest = function() {
+        
+    }
+
+})();
+
+
+
+
+
+let addMovieController  = (function() {
+
+})(UIController);
+
+
+
+
+
+let deleteMovieController  = (function() {
+
+})();
+
+
+
+
+let editMovieController  = (function() {
+
+})();
+
+
+
+
+
+
+let controller = (function(UIctrl, userCtrl) {
+    
+    let setupEventListeners = function() {
+        let DOM = UIctrl.getDOMStrings();
+
+        DOM.loginBtn.addEventListener('click', userCtrl);
+
+        console.log(DOM);
+    }
+    
+    return {
+        init: function() {
+            console.log('Application has started!');
+
+            setupEventListeners();
+
+        },
+    }
+
+})(UIController, userController);
+
+
+controller.init();
